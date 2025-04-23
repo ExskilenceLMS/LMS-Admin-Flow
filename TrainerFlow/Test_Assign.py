02668f88230f74bf06fcb1ad19092838cdf800db
tpsdevlop <developers@thoughtprocess.co.in>
2025-04-23 15:49:41 +0530

    23-04-25 by Rk 2

--- TrainerFlow/Test_Assign.py
+++ TrainerFlow/Test_Assign.py
@@ -24,6 +24,49 @@ def filter_for_assign_tests(request):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 @api_view(['POST'])
+def get_tests_details(request):
+    try:
+        data = json.loads(request.body)
+        filters = {}
+
+        if data.get('track') != "":
+            filters.update({'track_id__track_name':data.get('track')})
+        if data.get('subject')!= "":
+            filters.update({'subject_id__subject_name':data.get('subject')})
+        if data.get('topic')!= "":
+            filters.update({'topic_id__in':data.get('topic')})
+        if data.get('level')!= "":
+            filters.update({'level':data.get('level')})
+        if data.get('tags')!= "":
+            filters.update({'tags__in':data.get('tags')})
+        if data.get('marks')!= "":
+            filters.update({'test_marks':data.get('marks')})
+        if data.get('duration')!= "":
+            filters.update({'test_duration':data.get('duration')})
+        if data.get('date')!= "":
+            filters.update({'test_date_and_time__date':data.get('date')})
+
+        tests = test_details.objects.filter(**filters,test_date_and_time__gte=datetime.now(),del_row=False)
+        test_data = []
+        for test in tests:
+            test_data.append({
+                'test_id': test.test_id,
+                'title': test.test_name,
+                'description': test.test_description,
+                'duration': test.test_duration,
+                'marks': test.test_marks,
+                'subject': test.subject_id.subject_name if test.subject_id else None,
+                'date': test.test_date_and_time.date() if test.test_date_and_time else None,
+                'time': test.test_date_and_time.time()  if test.test_date_and_time else None,
+                'track': test.track_id.track_name if test.track_id else None,
+                'course': test.course_id.course_name if test.course_id else None,
+                'test_type': test.test_type if test.test_type else None
+
+            })
+        return JsonResponse(test_data, safe=False)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+@api_view(['POST'])
 def update_test_details(request):
     try:
         data = json.loads(request.body)
@@ -33,7 +76,7 @@ def update_test_details(request):
         test.course_id = course_model.objects.get(course_name = data.get('course'),del_row = False)
         test.subject_id = subject_model.objects.get(subject_name = data.get('subject'),del_row = False)
         test.test_date_and_time = datetime.strptime(data.get('date'), '%Y-%m-%d').__add__(timedelta(days=0, hours=int(data.get('time').split(':')[0]), minutes=int(data.get('time').split(':')[1]), seconds= int(data.get('time').split(':')[2])))
-        # test.save()
+        test.save()
         return JsonResponse({'message':'success'}, status=200)
 
     except Exception as e:
@@ -55,11 +98,16 @@ def filter_for_sorting_students(request,track):
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
 @api_view(['GET'])
-def get_students(request,course,batch):
+def get_students(request,course,batch,testID):
     try:
         students = list(student_model.objects.filter(batch_id__batch_name=batch,course_id__course_name=course,del_row=False).values(
                                         'student_id','student_firstname','student_lastname','student_type','college','branch'))
-        return JsonResponse(students,safe=False, status=200)
+        assigned_students = list(students_assessments.objects.using('mongodb').filter(test_id=testID,del_row=False).values('student_id'))
+        response ={
+            'students':students,
+            'assigned_students_ids':[i.get('student_id') for i in assigned_students]
+        }
+        return JsonResponse(response,safe=False, status=200)
 
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
@@ -68,9 +116,21 @@ def assign_tests(request):
     try:
         data = json.loads(request.body)
         test = test_details.objects.get(test_id = data.get('test_id'),del_row = False)
-        print("test.subject_id",test.subject_id)
+        assigned_students = data.get('assigned_students_ids',[])
+        old_Std = students_assessments.objects.using('mongodb').filter(test_id=test.test_id) 
+        del_std =[]
+        update_oldStudents =[]
         std_obj =[]
         for std in data.get('students_list',[]):
+            if std in assigned_students:
+                assigned_students.remove(std)
+                continue
+            if std in [i.student_id for i in old_Std]:
+                 for i in old_Std:
+                    if i.student_id == std:
+                        i.del_row = False
+                        del_std.append(i)
+                 continue
             student_test = students_assessments(
                 student_id = std,
                 assessment_type = test.test_type,
@@ -82,8 +142,18 @@ def assign_tests(request):
                 assessment_max_score = test.test_marks
             )
             std_obj.append(student_test)
-        assigned = students_assessments.objects.bulk_create(std_obj)
+        if len(std_obj) != 0:
+            assigned = students_assessments.objects.using('mongodb').bulk_create(std_obj)
+        if len(assigned_students) != 0:
+            for std in old_Std:
+                if std.student_id in assigned_students:
+                    std.del_row = True
+                del_std.append(std)
+        if len(del_std) != 0:
+            # students_assessments.objects.using('mongodb').bulk_update(del_std,fields=['del_row'])
+            for std in del_std:
+                std.save()
         return JsonResponse({"status": "success"})
     except Exception as e :
         print(e)
-        return JsonResponse({'erroe':str(e)})
+        return JsonResponse({'error':str(e)})
